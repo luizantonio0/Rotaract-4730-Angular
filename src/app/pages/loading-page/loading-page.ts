3ff3fc7d4d670a6f8a8ad5fdc222713a6297b9a0
Luiz Antônio Gonçalves <antonio.luiz@ufpr.br>
2025-08-18 21:01:45 -0300

    Add loading page component and integrate loading indicators in members, projects, and transparency portal pages

---
+++ src/app/pages/loading-page/loading-page.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit, signal } from '@angular/core';
+import { ProgressBarModule } from 'primeng/progressbar';
+// Test
+import { ToastModule } from 'primeng/toast';
+
+@Component({
+  selector: 'app-loading-page',
+  imports: [ProgressBarModule, ToastModule],
+  templateUrl: './loading-page.html',
+  styleUrl: './loading-page.css'
+})
+export class LoadingPage implements OnInit {
+@Input() width: string = "100dvw"
+
+loadingBar = signal<number>(0)
+  async ngOnInit(): Promise<void> {
+      for (let index = 0; index < 100; index++) {
+        const newValue = await this.loading(this.loadingBar());
+        this.loadingBar.set(newValue);
+      }
+  }
+  loading(num: number): Promise<number> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(num + 1);
+      }, 510);
+    });
+  }
+}
